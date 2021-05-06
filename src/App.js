6bee6ff42f7e3a930b71e898db5c034dad983eac
Stephen Yan <syan6457@gmail.com>
2021-05-06 04:36:41 -0400

    reverted back to working version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
-import Game from './Game';
-
-//const code = new URLSearchParams(window.location.search).get('code');
+const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
   return (
     <div className="App">
       {/* renders dashboard if a code is obtained, renders login otherwise*/}
-      {/* {code ? <Dashboard code={code}/> : <Login/> } */}
-      <Game/>
+      {code ? <Dashboard code={code}/> : <Login/> }
     </div>
   );
 }
