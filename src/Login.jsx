03176da209c5e2cb61852c155cd06e9c97c8758e
Stephen Yan <syan6457@gmail.com>
2021-05-07 03:33:27 -0400

    changed redirect_uri to hosted frontend

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,7 +5,7 @@ function Login(props) {
     const auth_endpoint = 'https://accounts.spotify.com/authorize'
     const clientID = '8fdd97d3ea674907a02f38f39f755391';
     const response_type = 'code';
-    const redirect_uri = 'http://localhost:3000/';
+    const redirect_uri = 'https://young-spotify.vercel.app';
     const scopes = ['streaming',
                     'user-read-email', 
                     'user-read-private', 
